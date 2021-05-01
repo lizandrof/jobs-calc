aace3b43caed570cead09c516d7b1e2bf3606cab
Lizandro <lizandrof@gmail.com>
2021-05-01 10:24:15 +0200

    fourth class

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 // Usando template engine
 server.set('view engine', 'ejs')
+
+// Mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
 
 // Habilitar arquivos estáticos
 server.use(express.static("public"))
@@ -14,4 +18,4 @@ server.use(express.urlencoded({ extended: true}))
 // routes
 server.use(routes)
 
-server.listen(3000, () => console.log('rodando'))
+server.listen(3000, () => console.log('servidor rodando'))
